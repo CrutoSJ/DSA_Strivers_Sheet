b8ac560cd0f9e5447008bbb03ac0ea896c787388
Sumit <jha.07sumit2003@gmail.com>
2024-07-24 16:21:37 +0530

    Did using maxHeap

---
+++ DSA_SDE_Sheet_191/KthLargestEle.cpp
@@ -0,0 +1,24 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>maxHeap;
+        for(int i=0; i<nums.size(); i++){
+            maxHeap.push(nums[i]);
+        }
+
+        for(int i=1; i<k; i++){
+            maxHeap.pop();
+        }
+        return maxHeap.top();
+    }
+};
+
